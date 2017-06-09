028815fdb036c547ae698c92425a90314f4815c1
Christina Cornell <cdhorn515@gmail.com>
2017-06-09 01:20:16 -0400

    coded if statement to loop through array of objects, and also loop for inner array

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 (function() {
   'use strict';
 
-  var formData = [
-    {
+
+  var formData = [{
       "type": "text",
       "label": "First Name",
       "id": "user-first-name",
@@ -35,8 +35,7 @@
       "label": "Select Language",
       "id": "user-language",
       "icon": "",
-      "options": [
-        {
+      "options": [{
           "label": "English",
           "value": "EN"
         },
@@ -81,36 +80,94 @@
     }
   ];
 
-var formNode = document.getElementById('signup');
+  var inputNode;
+    // create form for DOM from form id signup in html
+    var formNode = document.getElementById('signup');
 
-for (var i = 0; i < formData.length; i++) {
-  //create formData var for each object in arr (label is one of the properties)
-  var input = formData[i];
+  var titleNode = document.createElement('h1');
+
+  // titleNode.textContent = "Sign Up For My Web App";
+  // formNode.appendChild(titleNode);
+
+  //loop through the object
+  for (var i = 0; i < formData.length; i++) {
+    var input = formData[i];
+    console.log(formData[i]);
+    if (input.type === "text" || input.type === "tel" || input.type === "email") {
+     inputNode = document.createElement('input');
+     inputNode.setAttribute('type', input.type);
+   } else if (input.type === "textarea") {
+     inputNode = document.createElement('textarea');
+   } else if (input.type === "select") {
+      // loop through array in options)
+      for (let j = 0; j < input.options.length; j++) {
+        console.log(input.options);
+        if (input.options.Node === "label") {
+          inputNode = document.createElement('label');
+          inputNode.setAttribute('label', input.options.label);
+        } else {
+          inputNode = document.createElement('value');
+          inputNode.setAttribute('value', input.options.value);
+        }
+        inputNode = document.createElement('select');
+        inputNode.setAttribute('select', input.select);
+      }
+    }
+    // add attributes to inputNode (key/value pairs)
+
+    inputNode.setAttribute('id', input.id);
+    inputNode.setAttribute('icon', input.icon);
+
+    console.log(inputNode);
+    // add to formNode
+    formNode.appendChild(inputNode);
+  }
+
+formNode.setAttribute('display', block);
+
+
+
+// Hints -----------
+
+// Accessing specific properties.
+//formData[0].label // this will give us "First Name"
+// as you can see we access the first element in the array
+// with [0] and then grab the property "label" using the "." character
+
+// Looping
+// Sample of how to loop over the formData
+//for(let i=0; i<formData.length; i++){
+
+  // Check your dev tools console for what the items in formData have
+//  console.log(formData[i])
+
+//}
+
+
+
+
+
+  // inputNode.setAttribute("input", )
   //creating different Nodes of each obj in the array
-  var inputNode = document.createElement('input');
+  // var inputNode = document.createElement('input');
   // console.log(input);
 
-  var formNode = document.querySelector('form');
-  // add newly created form to form node
-  formNode.appendChild(inputNode);
-
-  console.log(formNode);
-  var typeNode = document.createElement('type');
+  // var typeNode = document.createElement('type');
   //add newly created type to inputNode
-  inputNode.appendChild(typeNode);
-  var labelNode = document.createElement('label');
-  inputNode.appendChild(labelNode);
-  var idNode = document.createElement('id');
-  inputNode.appendChild(idNode);
-  var iconNode = document.createElement('icon');
-  inputNode.appendChild(iconNode);
-  var optionsNode = document.createElement('options');
-  inputNode.appendChild(optionsNode);
+  // inputNode.appendChild(typeNode);
+  // var labelNode = document.createElement('label');
+  // inputNode.appendChild(labelNode);
+  // var idNode = document.createElement('id');
+  // inputNode.appendChild(idNode);
+  // var iconNode = document.createElement('icon');
+  // inputNode.appendChild(iconNode);
+  // var optionsNode = document.createElement('options');
+  // inputNode.appendChild(optionsNode);
 
   // console.log(typeNode);
 
 
-}
+
 
 
 
@@ -120,23 +177,59 @@ for (var i = 0; i < formData.length; i++) {
   // websitePageOutline = document.createElement('type');
   //
   // websitePageOutline.appendChild(input);
- // console.log(input);
- // this.type =
+  // console.log(input);
+  // this.type =
 
 
-// append label
+  // append label
 
   // console.log(newInput);
 
   //access key/value pair of each object
   // for (var j = 0; j < input.length; j++) {
-    // this.input = input[j];
-    // console.log(input);
+  // this.input = input[j];
+  // console.log(input);
   // }
   //each object has key/value pairs
   // create and append element in for loop
   //
-// }
+  // }
+
+
+
+
+
+
+
+
+
+  //  console.log(input.hasOwnProperty('input'));
+  //check which type it is and create correct input
+  // if (inputNode.hasOwnProperty() === 'text' || inputNode.hasOwnProperty() === 'email' || inputNode.hasOwnProperty() === 'tel'){
+  //   inputNode = document.createElement('input');
+  // } else { if(inputNode.hasOwnProperty() === 'textarea'){
+  //   inputNode = document.createElement('textarea');
+  // } else { inputNode = document.createElement('select');
+  // // console.log(inputNode);
+  // }
+  // add new input to parent element formNode
+  // }
+  //     inputNode.setAttribute('type', input.type);
+  //     inputNode.setAttribute('placeholder', input.label);
+  //     inputNode.setAttribute('id', input.id);
+  //     inputNode.setAttribute('icon', input.icon);
+  //
+  // formNode.appendChild(inputNode);
+  //       // formNode.appendChild(text);
+  //       // formNode.appendChild(email);
+  //       // formNode.appendChild(tel);
+  //       // formNode.appendChild(textarea);
+  //       // formNode.appendChild(select);
+  //       console.log(inputNode);
+  //   }
+
+  // var input = formData[i];
+
 
 
 
